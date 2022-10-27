60bf7b201aaf1e043549b2ef94fd8c9c793dc206
Yuki Chong <yukichong@Yukis-MacBook-Pro.local>
2022-10-27 15:22:10 -0500

    Week 5

---
+++ week_05/histogram.js
@@ -0,0 +1,63 @@
+// Simple Histogram
+
+const height = 400,
+    width = 600,
+    margin = ({ top: 25, right: 10, bottom: 50, left: 10 }),
+    padding = 1; // distance between each bin
+
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("viewBox", [0, 0, width, height]);
+
+d3.json('climate-jan.json').then((data) => {
+
+  const x = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.average)).nice()
+    .range([margin.left, width - margin.right]);
+  
+  const y = d3.scaleLinear()
+    .range([height - margin.bottom, margin.top])
+    .domain([0,10]); // manually assigning the values instead of dynamically
+    
+  svg.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom + 5})`)
+    .call(d3.axisBottom(x));
+
+  const binGroups = svg.append("g")
+    .attr("class", "bin-group");
+
+  const bins = d3.bin()
+    .thresholds(10)
+    .value(d => d.average)(data);
+
+  let g = binGroups.selectAll("g")
+    .data(bins)
+    .join("g");
+
+  g.append("rect")
+    .attr("x", d => x(d.x0) + (padding / 2)) // x0 is the first position of the bins
+    .attr("y", d => y(d.length)) 
+    .attr("width", d => x(d.x1) - x(d.x0) - padding)
+    .attr("height", d => height - margin.bottom - y(d.length))
+    .attr("fill", "steelblue");
+
+//  With animations
+//   g.append("rect")
+//     .attr("x", d => x(d.x0) + (padding / 2)) // x0 is the first position of the bins
+//     .attr("width", d => x(d.x1) - x(d.x0) - padding)
+//     .attr("y", height - margin.bottom) 
+//     .attr("height", 0)
+//     .attr("fill", "steelblue")
+//     .transition()
+//     .duration(750)
+//     .attr("y", d => y(d.length)) 
+//     .attr("height", d => height - margin.bottom - y(d.length));
+
+  g.append("text")
+    .text(d => d.length)
+    .attr("x", d => x(d.x0) + (x(d.x1) - x(d.x0)) / 2)
+    .attr("y", d => y(d.length) - 5)
+    .attr("text-anchor", "middle")
+    .attr("fill", "#333");
+
+});
---
+++ week_05/scatterplot.js
@@ -0,0 +1,62 @@
+let height = 400,
+    width = 600,
+    margin = ({ top: 25, right: 30, bottom: 35, left: 40 });
+  
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("viewBox", [0, 0, width, height]);
+
+d3.csv('penguins.csv').then(data => {
+  
+  let x = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.body_mass_g)).nice()
+    .range([margin.left, width - margin.right]);
+
+  let y = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.flipper_length_mm)).nice()
+    .range([height - margin.bottom, margin.top]);
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .attr("class", "x-axis")
+    .call(d3.axisBottom(x).tickFormat(d => (d/1000) + "kg").tickSize(-height + margin.top + margin.bottom)) 
+    // negative tickSize to draw all the way across to create grid
+
+  svg.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .attr("class", "y-axis")
+    .call(d3.axisLeft(y).tickSize(-width + margin.left + margin.right))
+
+  svg.append("g")
+    .attr("fill", "black")
+    .selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cx", d => x(d.body_mass_g)) // cx, cy is the positioning of circles
+    .attr("cy", d => y(d.flipper_length_mm))
+    .attr("r", 2) // r is radius
+    .attr("opacity", 0.75);
+
+  const tooltip = d3.select("body").append("div")
+    .attr("class", "svg-tooltip")
+    .style("position", "absolute")
+    .style("visibility", "hidden");
+
+  d3.selectAll("circle")
+    .on("mouseover", function(event, d) { // create an event that listens for mouseover
+      d3.select(this).attr("fill", "red");
+      tooltip
+        .style("visibility", "visible")
+        .html(`Species: ${d.species}<br />Island: ${d.island}<br />Weight: ${d.body_mass_g/1000}kg`);
+    })
+    .on("mousemove", function(event) { // create an event that listens for mousemove
+      tooltip
+        .style("top", (event.pageY - 10) + "px")
+        .style("left", (event.pageX + 10) + "px");
+    })
+    .on("mouseout", function() {
+      d3.select(this).attr("fill", "black");
+      tooltip.style("visibility", "hidden");
+    })
+    
+});
